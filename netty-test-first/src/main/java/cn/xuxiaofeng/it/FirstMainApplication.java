b7857e033bbaabe768f284b1f05a91b494c4650e
xuxiaofeng <369815926@qq.com>
2021-07-21 10:45:52 +0800

    feat: create new project.

---
+++ netty-test-first/src/main/java/cn/xuxiaofeng/it/FirstMainApplication.java
@@ -0,0 +1,17 @@
+package cn.xuxiaofeng.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: FirstMainApplication
+ * @Description: 启动类
+ * @CreateBy: xxf
+ * @Date: 2021/7/21 10:35
+ */
+@SpringBootApplication
+public class FirstMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMainApplication.class);
+    }
+}
